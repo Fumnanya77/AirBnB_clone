33038a9c2a823ba7109b6cb052d4fd8486362a60
Astro175 <aspark400@gmail.com>
2023-03-07 23:16:03 +0100

    set up the base model class for task 3

--- models/base_model.py
+++ models/base_model.py
@@ -6,8 +6,29 @@ from datetime import datetime
 
 class BaseModel:
     """Base class for other classes"""
-    def __init__(self,*args, **kwargs):
-        if kwargs:
-            for key, value in kwargs.items():
-                if key == 'created_at' or key == 'updated_at':
+    def __init__(self):
+        """
+          Initialising the id and updated time of instances
+        """
+        self.id = uuid.uuid4()
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
+    def __str__(self):
+        """
+          Makes the attribute as strings
+        """
+        return ("[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__))
+
+    def save(self):
+        """
+          Saves changes made to the objects
+        """
+        self.updated_at = datetime.now()
+    
+    def to_dict(self):
+        dict_copy = self.__dict__.copy()
+        dict_copy["__class__"] = self.__class__.__name__
+        dict_copy["created_at"] = self.created_at.isoformat()
+        dict_copy["updated_at"] = self.updated_at.isoformat()
+        return dict_copy
