a57ce26ecd57472b05d774eed3fed7f748154f6b
Astro175 <aspark400@gmail.com>
2023-03-08 20:57:11 +0100

    fixing bugs

--- models/base_model.py
+++ models/base_model.py
@@ -10,9 +10,16 @@ class BaseModel:
         """
           Initialising the id and updated time of instances
         """
-        self.id = uuid.uuid4()
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs is not None:
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    if key == 'updated_at' or key == 'created_at':
+                        value = datetime.fromisoformat(value)
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """
