638ad0a188b7d7480b02e9cce6594adbf60a37f8
Astro175 <aspark400@gmail.com>
2023-03-10 13:38:20 +0100

    fixing bugs in filestorage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -11,7 +11,7 @@ class FileStorage:
 
     """
     __file_path = "file.path"
-    __object = {}
+    __objects = {}
 
     def all(self):
         """returns a dictionary of all the objects"""
@@ -22,7 +22,7 @@ class FileStorage:
         Sets an object in the __object dictionary with key <obj class name>.id
         """
         key = "{}.{}".format(obj.__class__.__name__,obj.id)
-        FileStorage.__object[key] = obj
+        FileStorage.__objects[key] = obj
 
     def save(self):
         """serializes the __object to the json file"""
@@ -35,12 +35,12 @@ class FileStorage:
 
     def reload(self):
         """Deserializes a json file to __object"""
-        if os.path.isfile(FileStorage.__path):
+        if path.isfile(FileStorage.__file_path):
             with open(FileStorage.__file_path, 'r', encoding='utf-8') as f:
-                deserialized_obj = json.dump(f)
+                deserialized_obj = json.load(f)
             for key, value in deserialized_obj.items():
                 class_name, obj_id = key.split('.')
-                cls = models.classes[class_name]
-                obj = cls(**value)
-                FileStorage.__objects[key] = obj
+                print(type(value))
+                obj = eval(class_name)(**obj_dict)
+                self.__objects[key] = obj
             return FileStorage.__object
