9da53dbd73d845ade431f010f8ffb012efaa5037
= <=>
2023-03-10 17:56:46 -0800

    console bugs

--- console.py
+++ console.py
@@ -1,10 +1,29 @@
 #!/usr/bin/python3
 """Writing line-oriented command interpreters"""
 import cmd
+import json
+import sys
+from models import base_model
+from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
 
 class HBNBcommand(cmd.Cmd):
     """Creating a simple line-oriented commands"""
     prompt = '(hbnb) '
+
+    def do_create(self, line):
+        """Create new instances of `BaseModel` and store in JSON file"""
+        if line == "":
+            print("** class name missing **")
+        elif not line in dir(base_model):
+            print("** class doesn\'t exist **")
+        elif line in dir(base_model):
+            print(line)
+            obj = FileStorage.new(self, json.loads(line))
+            print(obj)
+            obj = FileStorage.save(self)
+            print(obj)
+            print(obj.id)
 
     def do_EOF(self, line):
         """End of file to exit the program"""
