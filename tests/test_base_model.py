c318253c2b8f8122493a34902b6e0a63f8f8a176
= <=>
2023-03-12 17:05:17 -0700

    unittesting

--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -2,13 +2,26 @@
 """Test Case for the `BaseModel`"""
 
 import unittest
+import sys
+sys.path.append('../../AirBnB_clone')
 from models.base_model import BaseModel
+from datetime import datetime
 
 
 class TestBaseModel(unittest.TestCase):
     """Test Cases on the `BaseModel` `class`"""
-	def test_kwargs(self):
+#    def test_init_kwargs(self):
+#        """Testing the `kwargs` input"""
+#        self.assertIsInstance(BaseModel, dict, "Dictionary only")
+#        self.assertIsNotNone(BaseModel)
+#        self.assertTrue(len(BaseModel), "Empty dictionary")
+
+    def test_to_dict(self):
+        """Testing the `dict` method"""
+        obj_dict = BaseModel.to_dict(self)
+        self.assertIsInstance(obj_dict, dict)
+        self.assertTrue(type(obj_dict[key]), (int, str))
 
 
-if __name__ = __main__:
+if __name__ == '__main__':
     unittest.main()
--- tests/test_file_storage.py
+++ tests/test_file_storage.py
@@ -2,14 +2,26 @@
 """Test Case for the `FileStorage`"""
 
 import unittest
+import sys
+sys.path.append('../../AirBnB_clone')
 from models.base_model import BaseModel
 from models.engine.file_storage import FileStorage
 
 
 class TestFileStorage(unittest.TestCase):
     """Test Cases on the `FileSorage` `class`"""
-	def test_dict(self):
+    def test_all(self):
+        """Test that `all` returns a dictionary"""
+        all_obj = FileStorage.all(self)
+        self.assertIsInstance(all_obj, dict)
+
+    def test_new(self):
+        """Test that `new` object is an instance of a class"""
+#        self.assertTrue(type(obj in FileSorage.new(self, obj)), object)
+
+    def test_reload(self):
+        """Test the `reload` method"""
 
 
-if __name__ = __main__:
+if __name__ == '__main__':
     unittest.main()
