85daa850d7b88b44187545a948eb440bb79a8793
= <=>
2023-03-13 16:26:59 -0700

    Testing is On!

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -14,6 +14,7 @@ class TestBaseModel(unittest.TestCase):
         self.obj = BaseModel()
 
     def test_init(self):
+        """To test the object attributes"""
         self.assertIsInstance(self.obj.id, str)
         self.assertIsInstance(self.obj.created_at, datetime)
 
@@ -22,12 +23,17 @@ class TestBaseModel(unittest.TestCase):
         self.assertEqual(self.obj.__str__, self.obj.__str__)
 
     def test_save(self):
+        """To test the save method"""
         self.assertIsInstance(self.obj.updated_at, datetime)
         self.assertFalse(self.obj.save(), "")
+        self.assertNotEqual(self.obj.updated_at, self.obj.save())
 
     def test_to_dict(self):
         """Testing the `dict` method"""
+        obj_dict = self.obj.to_dict()
         self.assertIsInstance(self.obj.to_dict(), dict)
+        self.assertTrue(self.obj.to_dict())
+        self.assertTrue(obj_dict['__class__'] == 'BaseModel')
 
 
 if __name__ == '__main__':
